2db52392d9aa81e8697ce9198403ab42ca5f37cb
Fernando Castro <fernandocastrogon@gmail.com>
2022-11-27 15:16:18 -0300

    REST Api em Spring boot

---
+++ src/test/java/com/estoque/crm/epicrmapi/EpicrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.crm.epicrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
