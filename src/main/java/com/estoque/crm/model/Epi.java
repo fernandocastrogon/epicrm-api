493adc8d6aaa47b05f610eae113efdf06b533461
Fernando Castro <fernandocastrogon@gmail.com>
2022-11-28 22:39:32 -0300

    Criando atributos validadeCa, categoria, quantidadeEstoque

--- src/main/java/com/estoque/crm/model/Epi.java
+++ src/main/java/com/estoque/crm/model/Epi.java
@@ -4,6 +4,7 @@ package com.estoque.crm.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 
 
 @Data
@@ -20,8 +21,14 @@ public class Epi {
     @Column(nullable = false)
     private int ca;
 
+    @Column(nullable = false)
+    private LocalDate validadeCa;
 
+    @Column(nullable = false)
+    private String categoria;
 
+    @Column(nullable = false)
+    private int quantidadeEstoque;
 
 
 }
